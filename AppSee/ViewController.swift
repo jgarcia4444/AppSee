bfed9135989d636f13589f7d00e54432ed17112f
Jake Garcia <bballwiz4@gmail.com>
2019-08-31 01:22:10 -0700

    Initial commit outlet and action set up to image view and camera icon once the UIImagePicker is closed it brings back the photo with it

--- AppSee/ViewController.swift
+++ AppSee/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            
+            
+        }
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        //initialize model
+        
+        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else {
+            fatalError("error intializing the model")
+        }
+        
+        
+        
+        
+        
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true)
+        
+    }
+    
 }
 
